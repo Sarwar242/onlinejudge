7956974651f8e54a9753c5c360083b3b9e482001
Sarwar <sarwar.cse4.bu@gmail.com>
2020-07-18 17:58:20 +0600

    virtual environment and Postgres DB setup

--- OnlineJudge/OnlineJudge/settings.py
+++ OnlineJudge/OnlineJudge/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'OnlineJudge.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'onlinejudgedb',
+        'USER':'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
     }
 }
 
