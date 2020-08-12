21ff5cf3eaff8a1b51cbe9021d8a20e20cb29e51
Sarwar <sarwar.cse4.bu@gmail.com>
2020-08-13 01:38:23 +0600

    Database Changed to sqlite

--- OnlineJudge/OnlineJudge/settings.py
+++ OnlineJudge/OnlineJudge/settings.py
@@ -86,11 +86,8 @@ WSGI_APPLICATION = 'OnlineJudge.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'onlinejudgedb',
-        'USER':'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
